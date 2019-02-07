d3e9199fdb2f073bf97b56643afc3ce116caaf7c
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-02-07 13:27:36 -0500

    trying to fix events

--- Blocmetrics/config/application.rb
+++ Blocmetrics/config/application.rb
@@ -11,9 +11,19 @@ module Blocmetrics
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'localhost:3000', /https*:\/\/.*?bloopist\.com/
+        resource '*', :headers => :any, :methods => :any
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
   end
 end
